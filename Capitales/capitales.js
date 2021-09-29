0184c791125ebb490a70fad23a33e167bce65cd7
Gamora19 <leomsara221@gmail.com>
2021-09-29 03:23:45 -0500

    segundo commit

---
+++ Capitales/capitales.js
@@ -0,0 +1,143 @@
+const ciudad = () => {
+    let ciudad = prompt("Ingresa una ciudad/capital");
+
+
+
+    switch (ciudad) {
+
+        case "Leticia":
+            alert("Amazonas");
+            break;
+
+        case "Medellín":
+            alert("Antioquia");
+            break;
+
+        case "Arauca":
+            alert("Arauca");
+            break;
+
+        case "barranquilla":
+            alert("Atlántico");
+            break;
+
+        case "Cartagena":
+            alert("Bolívar");
+            break;
+
+        case "Tunja":
+            alert("Boyacá");
+            break;
+
+        case "Manizales":
+            alert("Caldas");
+            break;
+
+        case "Florencia":
+            alert("Caquetá");
+            break;
+
+        case "Yopal":
+            alert("Casanare");
+            break;
+
+        case "Popayán":
+            alert("Cauca");
+            break;
+
+        case "Valledupar":
+            alert("Cesar");
+            break;
+
+        case "Quibdó":
+            alert("Chocó");
+            break;
+
+        case "Montería":
+            alert("Córdoba");
+            break;
+
+        case "Bogotá":
+            alert("Cundinamarca");
+            break;
+
+        case "Puerto InÍrida":
+            alert("Guainía");
+            break;
+
+        case "Riohacha":
+            alert("Guajira");
+            break;
+
+        case "San José del Guaviare":
+            alert("Guaviare");
+            break;
+
+        case "Neiva":
+            alert("Huila");
+            break;
+
+        case "Santa Marta":
+            alert("Magdalena");
+            break;
+
+
+        case "Villavicencio":
+            alert("Meta");
+            break;
+
+        case "Pasto":
+            alert("Nariño");
+            break;
+
+        case "Cúcuta":
+            alert("Norte de Santander");
+            break;
+
+        case "Mocoa":
+            alert("Putumayo");
+            break;
+
+        case "Armenia":
+            alert("Quindío");
+            break;
+
+        case "Pereira":
+            alert("Risaralda");
+            break;
+
+        case "San Andrés":
+            alert("San Andrés y Providencia");
+            break;
+
+        case "Bucaramanga":
+            alert("Santander");
+            break;
+
+        case "Sincelejo":
+            alert("Sucre");
+            break;
+
+        case "Ibagué":
+            alert("Tolima");
+            break;
+
+        case "Cali":
+            alert("Valle del Cauca");
+            break;
+
+        case "Mitú":
+            alert("Vaupés");
+            break;
+
+        case "Puerto Carreño":
+            alert("Vichada");
+            break;
+
+        default:
+            alert("Asegurese que la ciudad/capital este escrita correctamente");
+            break;
+
+    }
+}
+
